d3f32ad71c4f2b262be986080bf57cb4d7c50faa
Amey Patankar <ameypatankar333@gmail.com>
2025-12-12 18:33:03 +0530

    Time: 6 ms (67.35%) | Memory: 63.9 MB (58.49%) - LeetSync

--- 9-palindrome-number/palindrome-number.js
+++ 9-palindrome-number/palindrome-number.js
@@ -3,12 +3,13 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
+    if(x <0) return false;
     let rev = 0;
     let rem = 0;
     let xCopy = x;
     while(x>0) {
         let rem = x %10;
-        rev = (rev*10) + rem;
+        rev = (rev*10) + rem; //
         x = Math.floor(x/10);
     }
     return rev === xCopy;
