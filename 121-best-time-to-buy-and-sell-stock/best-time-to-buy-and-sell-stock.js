ee8927f5cff5a3ce4747193674a0b5966e165233
Amey Patankar <ameypatankar333@gmail.com>
2025-12-25 16:33:15 +0530

    Time: 0 ms (100.00%) | Memory: 65.6 MB (61.02%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -3,16 +3,15 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-  let min = prices[0];
+    let min = prices[0];
     let max_profit = 0;
     for (let i = 0; i < prices.length; i++) {
         if (min > prices[i]) {
             min = prices[i];
         }
         if (min < prices[i]) {
-            let minus = prices[i] - min;
-            if (minus > max_profit) {
-                max_profit = minus;
+            if (prices[i] - min > max_profit) {
+                max_profit = prices[i] - min;
             }
         }
     }
